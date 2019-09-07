d755bedc4ad6375ac5f432fe84e2798d931f3f9a
FernandesMF <mario.fog.fernandes@gmail.com>
2019-09-07 15:38:05 -0300

    implemented json file reading

--- main.cpp
+++ main.cpp
@@ -1,17 +1,32 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
 #include "Timeliner.h"
 
-#pragma once
+#define events_file "events.json"
+
+json ReadJson(std::string);
 
 int main(){
 
-    json events = NULL;
+    json Events = NULL;
 
-    // read events file
-    // transform them into a timeline
+    Events = ReadJson(events_file);
+
+    std::cout << std::setw(4) << Events<< std::endl;
+
+    // transform teh events into a timeline
     // write timeline to external file
     // print timeline to screen?
 
-    // test a commit, please
-
     return 0;
+}
+
+// read events file, store in json variable Events
+json ReadJson(std::string FileName){
+    json Events = NULL;
+    std::ifstream EventsFile(FileName, std::ifstream::binary);
+    EventsFile >> Events;
+
+    return Events;
 }
