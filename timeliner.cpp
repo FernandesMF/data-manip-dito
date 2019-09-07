f163d6c0d8f026ffe0aeb60a48facaa5a7b1739f
FernandesMF <mario.fog.fernandes@gmail.com>
2019-09-07 18:27:09 -0300

    references to spare space; only main file

---
+++ timeliner.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include "nlohmann/json.hpp"
+//#include "Timeliner.cpp"
+
+using json = nlohmann::json;
+
+// TODO main: use terminal arguments for input and output
+#define INPUT_FILE "events.json"
+#define OUTPUT_FILE "timeline.json"
+
+
+
+json ReadJson(std::string);
+void WriteJson(std::string, json);
+void PrintJson(json);
+void Timeliner(json &, json &);
+
+
+
+int main(){
+
+    json Events = NULL;
+    json Timeline = NULL;
+    json &EventsRef = Events;
+    json &TimelineRef = Timeline;
+
+    Events = ReadJson(INPUT_FILE);
+    PrintJson(Events);
+    Timeliner(EventsRef,TimelineRef);
+    PrintJson(Timeline);
+    WriteJson(OUTPUT_FILE, Timeline);
+
+    return 0;
+}
+
+
+
+void PrintJson(json JsonData){
+    std::cout << std::setw(4) << JsonData << std::endl;
+}
+
+// TODO json reader: file opening/ existence checks
+json ReadJson(std::string FileName){
+    json JsonData = NULL;
+    std::ifstream EventsFile(FileName, std::ifstream::binary);
+    EventsFile >> JsonData;
+
+    return JsonData;
+}
+
+//TODO json writer: file opening checks
+void WriteJson(std::string FileName,json JsonData){
+    std::ofstream TimelineFile;
+    TimelineFile.open(OUTPUT_FILE);
+    TimelineFile << JsonData;
+    TimelineFile.close();
+
+    return;
+}
+
+
+
+void Timeliner(json &BuyData, json &Timeline){
+
+    // get group of events with the same trans. id as the first
+    // transfer it/them to Timeline (insert at the right place, remove from BuyData)
+    // repeat until BuyData is empty
+
+    Timeline = BuyData;      // FIXME this is just dummy code;
+
+    return;
+}
